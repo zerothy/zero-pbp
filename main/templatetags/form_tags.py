97d28f79651907666f08a316f51cc212006a9145
zerothy <zxuyagaming@gmail.com>
2024-10-01 14:41:52 +0700

    feat: register page

--- main/templatetags/form_tags.py
+++ main/templatetags/form_tags.py
@@ -4,4 +4,6 @@ register = template.Library()
 
 @register.filter(name='add_class')
 def add_class(field, css):
-    return field.as_widget(attrs={"class": css})
+    if hasattr(field, 'as_widget'):
+        return field.as_widget(attrs={"class": css})
+    return field
